41006bbf068ca3592b19b52b01a9add6ade14982
tyler <kolatra03@gmail.com>
2023-08-19 00:59:33 -0600

    add ip info to messages

--- src/webhook.rs
+++ src/webhook.rs
@@ -3,22 +3,22 @@ use std::net::IpAddr;
 use webhook::client::WebhookClient;
 
 pub fn log_mc_ping(address: &str, target: &str) {
-    let msg = format!("minecraft ping from {address} to {target}");
+    let msg = format!("minecraft ping from {} to {target}", ip_info(address));
     send(msg)
 }
 
 pub fn log_connection(address: &str) {
-    let msg = format!("non-minecraft connection from {address}");
+    let msg = format!("non-minecraft connection from {}", ip_info(address));
     send(msg)
 }
 
 pub fn log_join(address: IpAddr, user: &str) {
-    let msg = format!("({address}) {user} joined the server");
+    let msg = format!("({}) {user} joined the server", ip_info(&address.to_string()));
     send(msg)
 }
 
 pub fn log_leave(address: IpAddr, user: &str) {
-    let msg = format!("({address}) {user} disconnected");
+    let msg = format!("({}) {user} disconnected", ip_info(&address.to_string()));
     send(msg)
 }
 
@@ -34,4 +34,22 @@ fn send(msg: String) {
 
         Ok::<(), anyhow::Error>(())
     });
+}
+
+fn ip_info(mut ip: &str) -> String {
+    if let Some(i) = ip.find(':') {
+        ip = &ip[..i]
+    };
+
+    format!("[`{}`](<https://ipinfo.io/{}>)", ip, ip)
+}
+
+#[test]
+fn test_ip_info() {
+    let ip = "192.168.1.74";
+
+    assert_eq!(
+        ip_info(&format!("{}:25565", ip)),
+        format!("[`{}`](<https://ipinfo.io/{}>)", ip, ip)
+    );
 }
